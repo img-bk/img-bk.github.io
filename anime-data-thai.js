c6ddecf179f49d6bdaff57998e7b83e5b7899d52
img-bk <t0766054@gmail.com>
2025-07-14 15:00:49 +0700

    Create anime-data-thai.js

---
+++ anime-data-thai.js
@@ -0,0 +1,38 @@
+const animeList = [
+
+  {
+    title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/bf26af8a102711cdc85dc1befc400c4e.jpg",
+    type: "ตอนที่ 1-170 พากย์ไทย",
+    link: "https://blackcloverth.github.io/"
+  },
+
+  {
+  
+    title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-216 พากย์ไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
+    type: "ตอนที่ 1-216 พากย์ไทย",
+    link: "https://perfectxworld.github.io"
+  },
+    {
+  
+    title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-107 พากย์ไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
+    type: "ตอนที่ 1-107 พากย์ไทย Ss2",
+    link: "https://douluo-dalu.github.io/thai"
+  },
+  
+  
+  {
+  
+    title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-178 พากย์ไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",
+    type: "ตอนที่ 1-178 พากย์ไทย",
+    link: "https://tunshi-xingkong.github.io/thai"
+  },
+
+    
+        
+
+  // เพิ่มรายการอนิเมะได้เรื่อย ๆ
+];
