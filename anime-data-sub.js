62832028563d71688b4c7e564194a0e09354f23d
img-bk <t0766054@gmail.com>
2025-07-14 15:02:46 +0700

    Create anime-data-sub.js

---
+++ anime-data-sub.js
@@ -0,0 +1,38 @@
+const animeList = [
+    {
+  
+    title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-181 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",
+    type: "ตอนที่ 1-181 ซับไทย",
+    link: "https://tunshi-xingkong.github.io"
+  },
+    {
+    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-156 ซับไทย",
+    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
+    type: "ตอนที่ 1-156 ซับไทย Ss5",
+    link: "https://fights-break-sphere-season-5.github.io/"
+  },
+ 
+  
+  {
+    title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-224 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
+    type: "ตอนที่ 1-224 ซับไทย",
+    link: "https://perfectxworld.github.io/sub"
+  },
+
+  {
+  
+    title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-110 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
+    type: "ตอนที่ 1-110 ซับไทย Ss2",
+    link: "https://douluo-dalu.github.io/"
+  },
+  
+
+
+    
+        
+
+  // เพิ่มรายการอนิเมะได้เรื่อย ๆ
+];
