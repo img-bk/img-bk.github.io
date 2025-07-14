f500d997cd1ac3770c0efd2057fe721e19ee7b60
img-bk <t0766054@gmail.com>
2025-07-14 14:33:15 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -10,93 +10,43 @@ const animeList = [
     title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-156 ซับไทย",
     img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
     type: "ตอนที่ 1-156 ซับไทย Ss5",
-    link: "Fights-Break-Sphere-Season-5"
+    link: "https://fights-break-sphere-season-5.github.io/"
   },
   {
     title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/bf26af8a102711cdc85dc1befc400c4e.jpg",
     type: "ตอนที่ 1-170 พากย์ไทย",
-    link: "black-clover-thai"
-  },
-  {
-    title: "One Piece Season 21 – วันพีช ซีซั่น 21 เกาะอนาคตเกาะเอ็กเฮด ตอนที่ 1089-1137 ซับไทย",
-    img: "https://img2.pic.in.th/pic/One-Piece-Season-21-4.jpg",
-    type: "ตอนที่ 1089-1137 ซับไทย",
-    link: "One-Piece-Season-21"
-  },
-
-  {
-    title: "Fights Break Sphere Season 4 สัประยุทธ์ทะลุฟ้า ภาค 4 ตอนที่ 1-24 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-4-1_11zon.jpg",
-    type: "ตอนที่ 1-24 ซับไทย Ss4",
-    link: "Fights-Break-Sphere-Season4"
-  },
-  {
-    title: "Fights Break Sphere Season 3 สัประยุทธ์ทะลุฟ้า ภาค 3 ตอนที่ 1-12 ซับไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/Fights-Break-Sphere-Season-3-1-1.jpg",
-    type: "ตอนที่ 1-12 ซับไทย Ss3",
-    link: "Fights-Break-Sphere-Season-3"
-  },
-  {
-    title: "Fights Break Sphere Season 2 สัประยุทธ์ทะลุฟ้า ภาค 2 ตอนที่ 1-12 ซับไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/Fights-Break-Sphere-Season-2-1-1.jpg",
-    type: "ตอนที่ 1-12 ซับไทย Ss2",
-    link: "Fights-Break-Sphere-Season-2"
-  },
-  {
-    title: "Fights Break Sphere Season 1 สัประยุทธ์ทะลุฟ้า ภาค 1 ตอนที่ 1-12 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-e1701447291695-1-1.jpg",
-    type: "ตอนที่ 1-12 ซับไทย Ss1",
-    link: "Fights-Break-Sphere-Season-1"
+    link: "https://blackcloverth.github.io/"
   },
   
-  {
-    title: "One Piece Season 21 – วันพีช ซีซั่น 21 เกาะอนาคตเกาะเอ็กเฮด ตอนที่ 1089-1122 พากย์ไทย",
-    img: "https://img2.pic.in.th/pic/One-Piece-Season-21-4.jpg",
-    type: "ตอนที่ 1089-1122 พากย์ไทย",
-    link: "One-Piece-Season-21-thai"
-  },
   {
     title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-224 ซับไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
     type: "ตอนที่ 1-224 ซับไทย",
-    link: "Perfect-World"
+    link: "https://perfectxworld.github.io/sub"
   },
   {
   
     title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-216 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
     type: "ตอนที่ 1-216 พากย์ไทย",
-    link: "Perfect-World-thai"
+    link: "https://perfectxworld.github.io"
   },
     {
   
     title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-107 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
     type: "ตอนที่ 1-107 พากย์ไทย Ss2",
-    link: "Douluo-Dalu-2-thai"
+    link: "https://douluo-dalu.github.io/thai"
   },
   {
   
     title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-110 ซับไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
     type: "ตอนที่ 1-110 ซับไทย Ss2",
-    link: "Douluo-Dalu-2"
+    link: "https://douluo-dalu.github.io/"
   },
-  {
   
-    title: "Douluo Dalu (Soul Land) ตำนานจอมยุทธ์ภูตถังซาน ตอนที่ 1-265 พากย์ไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/images-2-1bc9fc8f7380b3ec3.jpeg",
-    type: "ตอนที่ 1-265 พากย์ไทย",
-    link: "Douluo-Dalu"
-  },
-  {
-  
-    title: "Shen Yin Wangzuo ผนึกเทพบัลลังก์ราชันย์ ตอนที่ 1-168 ซับไทย",
-    img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",
-    type: "ตอนที่ 1-168 ซับไทย",
-    link: "Shen-Yin-Wangzuo"
-  },
   {
   
     title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-178 พากย์ไทย",
