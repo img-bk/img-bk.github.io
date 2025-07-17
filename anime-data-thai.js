e66318580f0e2c0e328ad0fcb252f46a74d8b364
img-bk <t0766054@gmail.com>
2025-07-18 01:22:09 +0700

    Update anime-data-thai.js

--- anime-data-thai.js
+++ anime-data-thai.js
@@ -1,12 +1,30 @@
 const animeList = [
-
+    {
+  
+    title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-181 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",
+    type: "ตอนที่ 1-181 ซับไทย",
+    link: "https://tunshi-xingkong.github.io"
+  },
+    {
+    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-156 ซับไทย",
+    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
+    type: "ตอนที่ 1-156 ซับไทย Ss5",
+    link: "https://fights-break-sphere-season-5.github.io/"
+  },
   {
     title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/bf26af8a102711cdc85dc1befc400c4e.jpg",
     type: "ตอนที่ 1-170 พากย์ไทย",
     link: "https://blackcloverth.github.io/"
   },
-
+  
+  {
+    title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-224 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
+    type: "ตอนที่ 1-224 ซับไทย",
+    link: "https://perfectxworld.github.io/sub"
+  },
   {
   
     title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-216 พากย์ไทย",
@@ -21,7 +39,13 @@ const animeList = [
     type: "ตอนที่ 1-107 พากย์ไทย Ss2",
     link: "https://douluo-dalu.github.io/thai"
   },
+  {
   
+    title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-110 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
+    type: "ตอนที่ 1-110 ซับไทย Ss2",
+    link: "https://douluo-dalu.github.io/"
+  },
   
   {
   
