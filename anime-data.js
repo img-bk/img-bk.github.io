c31aa6da15cbf69655fad2228b18915b0d8e019a
img-bk <t0766054@gmail.com>
2025-07-18 13:24:46 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -54,9 +54,52 @@ const animeList = [
     type: "ตอนที่ 1-178 พากย์ไทย",
     link: "https://tunshi-xingkong.github.io/thai"
   },
+      {
+  
+    title: "Shen Yin Wangzuo ผนึกเทพบัลลังก์ราชันย์ ตอนที่ 1-167 ซับไทย",
+    img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",
+    type: "ตอนที่ 1-167 ซับไทย",
+    link: "Shen-Yin-Wangzuo"
+  },
 
-    
+    {
+  
+    title: "Lookism คนมันหล่อช่วยไม่ได้ ตอนที่ 1-8 พากย์ไทย",
+    img: "https://img2.pic.in.th/pic/Lookism-1.jpg",
+    type: "ตอนที่ 1-8 พากย์ไทย",
+    link: "lookism-thai"
+  },  
         
 
-  // เพิ่มรายการอนิเมะได้เรื่อย ๆ
+
+  {
+  
+    title: "Blue Lock ขังดวลแข้ง ตอนที่ 1-24 พากย์ไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Blue-Lock--300x450-1.jpg",
+    type: "ตอนที่ 1-24 พากย์ไทย",
+    link: "blue-lock-thai-2"
+  },
+  {
+  
+    title: "Blue Lock ขังดวลแข้ง ตอนที่ 1-24 ซับไทย",
+    img: "https://img5.pic.in.th/file/secure-sv1/Blue-Lock--300x450-1.jpg",
+    type: "ตอนที่ 1-24 ซับไทย",
+    link: "blue-lock-sub"
+  },
+  {
+  
+    title: "Blue Lock vs. U-20 Japan – ขังดวลแข้ง ภาค 2 ตอนที่ 1-14 พากย์ไทย",
+    img: "https://img2.pic.in.th/pic/Blue-Lock-2nd-Season_11zon-1.jpg",
+    type: "ตอนที่ 1-14 พากย์ไทย Ss2",
+    link: "blue-lock-vs-u-20-japan-ss2-thai"
+  },
+  {
+  
+    title: "Blue Lock vs. U-20 Japan – ขังดวลแข้ง ภาค 2 ตอนที่ 1-14 ซับไทย",
+    img: "https://img2.pic.in.th/pic/Blue-Lock-2nd-Season_11zon-1.jpg",
+    type: "ตอนที่ 1-14 ซับไทย Ss2",
+    link: "blue-lock-vs-u-20-japan-ss2-sub"
+  },
+
+    // เพิ่มรายการอนิเมะได้เรื่อย ๆ
 ];
