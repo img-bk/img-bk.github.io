1b0c85e32b01e37a980bf131bcf04b6e08a6c36e
img-bk <t0766054@gmail.com>
2025-07-18 14:32:43 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -1,7 +1,7 @@
 const animeList = [
     {
   
-    title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-181 ซับไทย",
+    title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-181 จีนซับไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",
     type: "ตอนที่ 1-181 ซับไทย",
     link: "https://tunshi-xingkong.github.io"
@@ -9,7 +9,7 @@ const animeList = [
     {
     title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-156 ซับไทย",
     img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
-    type: "ตอนที่ 1-156 ซับไทย Ss5",
+    type: "ตอนที่ 1-156 จีนซับไทย Ss5",
     link: "https://fights-break-sphere-season-5.github.io/"
   },
   {
@@ -22,28 +22,28 @@ const animeList = [
   {
     title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-224 ซับไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
-    type: "ตอนที่ 1-224 ซับไทย",
+    type: "ตอนที่ 1-224 จีนซับไทย",
     link: "https://perfectxworld.github.io/sub"
   },
   {
   
     title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-216 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
-    type: "ตอนที่ 1-216 พากย์ไทย",
+    type: "ตอนที่ 1-216 จีนพากย์ไทย",
     link: "https://perfectxworld.github.io"
   },
     {
   
     title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-107 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
-    type: "ตอนที่ 1-107 พากย์ไทย Ss2",
+    type: "ตอนที่ 1-107 จีนพากย์ไทย Ss2",
     link: "https://douluo-dalu.github.io/thai"
   },
   {
   
     title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-110 ซับไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
-    type: "ตอนที่ 1-110 ซับไทย Ss2",
+    type: "ตอนที่ 1-110 จีนซับไทย Ss2",
     link: "https://douluo-dalu.github.io/"
   },
   
@@ -51,14 +51,14 @@ const animeList = [
   
     title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-178 พากย์ไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",
-    type: "ตอนที่ 1-178 พากย์ไทย",
+    type: "ตอนที่ 1-178 จีนพากย์ไทย",
     link: "https://tunshi-xingkong.github.io/thai"
   },
       {
   
     title: "Shen Yin Wangzuo ผนึกเทพบัลลังก์ราชันย์ ตอนที่ 1-167 ซับไทย",
     img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",
-    type: "ตอนที่ 1-167 ซับไทย",
+    type: "ตอนที่ 1-167 จีนซับไทย",
     link: "Shen-Yin-Wangzuo"
   },
 
