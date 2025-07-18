22117b79bf957773ca718996a59e84a368834a55
img-bk <t0766054@gmail.com>
2025-07-18 14:06:22 +0700

    Update anime-data-sub.js

--- anime-data-sub.js
+++ anime-data-sub.js
@@ -1,59 +1,116 @@
 const animeList = [
-    {
-  
-    title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-181 ซับไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",
-    type: "ตอนที่ 1-181 ซับไทย",
-    link: "https://tunshi-xingkong.github.io"
-  },
-    {
-    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-156 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
-    type: "ตอนที่ 1-156 ซับไทย Ss5",
-    link: "https://fights-break-sphere-season-5.github.io/"
-  },
- 
-  
-  {
-    title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-224 ซับไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
-    type: "ตอนที่ 1-224 ซับไทย",
-    link: "https://perfectxworld.github.io/sub"
-  },
+{
 
-  {
-  
-    title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-110 ซับไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",
-    type: "ตอนที่ 1-110 ซับไทย Ss2",
-    link: "https://douluo-dalu.github.io/"
-  },
-    {
-  
-    title: "Shen Yin Wangzuo ผนึกเทพบัลลังก์ราชันย์ ตอนที่ 1-168 ซับไทย",
-    img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",
-    type: "ตอนที่ 1-168 ซับไทย",
-    link: "Shen-Yin-Wangzuo"
-  },
+title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-181 ซับไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",  
+type: "ตอนที่ 1-181 ซับไทย",  
+link: "https://tunshi-xingkong.github.io"
 
+},
+{
+title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-156 ซับไทย",
+img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
+type: "ตอนที่ 1-156 ซับไทย Ss5",
+link: "https://fights-break-sphere-season-5.github.io/"
+},
+{
+title: "Black Clover แบล็คโคลเวอร์ ตอนที่ 1-170 พากย์ไทย",
+img: "https://img5.pic.in.th/file/secure-sv1/bf26af8a102711cdc85dc1befc400c4e.jpg",
+type: "ตอนที่ 1-170 พากย์ไทย",
+link: "https://blackcloverth.github.io/"
+},
 
-  {
-  
-    title: "Blue Lock ขังดวลแข้ง ตอนที่ 1-24 ซับไทย",
-    img: "https://img5.pic.in.th/file/secure-sv1/Blue-Lock--300x450-1.jpg",
-    type: "ตอนที่ 1-24 ซับไทย",
-    link: "blue-lock-sub"
-  },
+{
+title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-224 ซับไทย",
+img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",
+type: "ตอนที่ 1-224 ซับไทย",
+link: "https://perfectxworld.github.io/sub"
+},
+{
 
-  {
-  
-    title: "Blue Lock vs. U-20 Japan – ขังดวลแข้ง ภาค 2 ตอนที่ 1-14 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Blue-Lock-2nd-Season_11zon-1.jpg",
-    type: "ตอนที่ 1-14 ซับไทย Ss2",
-    link: "blue-lock-vs-u-20-japan-ss2-sub"
-  },
-    
-        
+title: "Perfect World โลกอันสมบูรณ์แบบ ตอนที่ 1-216 พากย์ไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Perfect-World-1.jpg",  
+type: "ตอนที่ 1-216 พากย์ไทย",  
+link: "https://perfectxworld.github.io"
 
-  // เพิ่มรายการอนิเมะได้เรื่อย ๆ
+},
+{
+
+title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-107 พากย์ไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",  
+type: "ตอนที่ 1-107 พากย์ไทย Ss2",  
+link: "https://douluo-dalu.github.io/thai"
+
+},
+{
+
+title: "Douluo Dalu II: Jueshi Tangmen ตำนานจอมยุทธ์ภูตถังซาน 2: สำนักถังเลิศภพจบแดน ตอนที่ 1-110 ซับไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Soul-Land-2-Peerless-Tang-Sect-768x1077_11zon.webp",  
+type: "ตอนที่ 1-110 ซับไทย Ss2",  
+link: "https://douluo-dalu.github.io/"
+
+},
+
+{
+
+title: "Tunshi Xingkong มหาศึกล้างพิภพ ตอนที่ 1-178 พากย์ไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Tunshi-Xingkong-1-1.jpg",  
+type: "ตอนที่ 1-178 พากย์ไทย",  
+link: "https://tunshi-xingkong.github.io/thai"
+
+},
+{
+
+title: "Shen Yin Wangzuo ผนึกเทพบัลลังก์ราชันย์ ตอนที่ 1-167 ซับไทย",  
+img: "https://img2.pic.in.th/pic/GoOnlineTools-image-downloader-1_11zon-1.jpeg",  
+type: "ตอนที่ 1-167 ซับไทย",  
+link: "Shen-Yin-Wangzuo"
+
+},
+
+{  
+
+title: "Lookism คนมันหล่อช่วยไม่ได้ ตอนที่ 1-8 พากย์ไทย",  
+img: "https://img2.pic.in.th/pic/Lookism-1.jpg",  
+type: "ตอนที่ 1-8 พากย์ไทย",  
+link: "lookism-thai"
+
+},
+
+{
+
+title: "Blue Lock ขังดวลแข้ง ตอนที่ 1-24 พากย์ไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Blue-Lock--300x450-1.jpg",  
+type: "ตอนที่ 1-24 พากย์ไทย",  
+link: "blue-lock-thai-2"
+
+},
+{
+
+title: "Blue Lock ขังดวลแข้ง ตอนที่ 1-24 ซับไทย",  
+img: "https://img5.pic.in.th/file/secure-sv1/Blue-Lock--300x450-1.jpg",  
+type: "ตอนที่ 1-24 ซับไทย",  
+link: "blue-lock-sub"
+
+},
+{
+
+title: "Blue Lock vs. U-20 Japan – ขังดวลแข้ง ภาค 2 ตอนที่ 1-14 พากย์ไทย",  
+img: "https://img2.pic.in.th/pic/Blue-Lock-2nd-Season_11zon-1.jpg",  
+type: "ตอนที่ 1-14 พากย์ไทย Ss2",  
+link: "blue-lock-vs-u-20-japan-ss2-thai"
+
+},
+{
+
+title: "Blue Lock vs. U-20 Japan – ขังดวลแข้ง ภาค 2 ตอนที่ 1-14 ซับไทย",  
+img: "https://img2.pic.in.th/pic/Blue-Lock-2nd-Season_11zon-1.jpg",  
+type: "ตอนที่ 1-14 ซับไทย Ss2",  
+link: "blue-lock-vs-u-20-japan-ss2-sub"
+
+},
+
+// เพิ่มรายการอนิเมะได้เรื่อย ๆ
+
 ];
+
